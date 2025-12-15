315ca256d6c2c2eb116c0e7573a371af0198b4eb
Simon Öztürk <info@scs-technik.de>
2025-12-15 21:25:37 +0100

    fixing github action

--- database/migrations/20251212_drop_kvp_points_table.sql
+++
@@ -1,9 +0,0 @@
--- Migration: Drop kvp_points table and related ENUM type
--- Date: 2025-12-12
--- Description: Remove dead KVP points system - table is empty and functionality unused
-
--- Drop the table first (depends on the ENUM type)
-DROP TABLE IF EXISTS kvp_points;
-
--- Drop the ENUM type
-DROP TYPE IF EXISTS kvp_points_reason;
