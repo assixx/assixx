853e39196f612985ab819d56c556165a989d6430
Simon Öztürk <info@scs-technik.de>
2026-01-28 22:22:33 +0100

    refactor: split calenchatdar service

--- backend/src/nest/chat/chat.module.ts
+++ backend/src/nest/chat/chat.module.ts
@@ -8,6 +8,9 @@
 import { Module } from '@nestjs/common';
 
 import { DocumentsModule } from '../documents/documents.module.js';
+import { ChatConversationsService } from './chat-conversations.service.js';
+import { ChatMessagesService } from './chat-messages.service.js';
+import { ChatScheduledService } from './chat-scheduled.service.js';
 import { ChatController } from './chat.controller.js';
 import { ChatService } from './chat.service.js';
 import { ScheduledMessageProcessorService } from './scheduled-message-processor.service.js';
@@ -15,7 +18,13 @@ import { ScheduledMessageProcessorService } from './scheduled-message-processor.
 @Module({
   imports: [DocumentsModule],
   controllers: [ChatController],
-  providers: [ChatService, ScheduledMessageProcessorService],
+  providers: [
+    ChatConversationsService,
+    ChatMessagesService,
+    ChatScheduledService,
+    ChatService,
+    ScheduledMessageProcessorService,
+  ],
   exports: [ChatService],
 })
 // eslint-disable-next-line @typescript-eslint/no-extraneous-class -- NestJS module pattern requires empty class
