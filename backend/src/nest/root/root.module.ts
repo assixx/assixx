f344594feb578af2e1a660dbd6e4e03d5d712a0b
Simon Öztürk <159275552+shimscho@users.noreply.github.com>
2026-01-29 16:02:04 +0100

    refactor: root service

--- backend/src/nest/root/root.module.ts
+++ backend/src/nest/root/root.module.ts
@@ -3,12 +3,20 @@
  */
 import { Module } from '@nestjs/common';
 
+import { RootAdminService } from './root-admin.service.js';
+import { RootDeletionService } from './root-deletion.service.js';
+import { RootTenantService } from './root-tenant.service.js';
 import { RootController } from './root.controller.js';
 import { RootService } from './root.service.js';
 
 @Module({
   controllers: [RootController],
-  providers: [RootService],
+  providers: [
+    RootService,
+    RootAdminService,
+    RootTenantService,
+    RootDeletionService,
+  ],
   exports: [RootService],
 })
 // eslint-disable-next-line @typescript-eslint/no-extraneous-class -- NestJS modules are decorator-configured
